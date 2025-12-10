a0e54861f4ab51ecb3371073fbf10513087bea96
Fares Bakhet <faresak47roo@gmail.com>
2025-12-10 19:09:50 +0000

    Completing level-100

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = ""; // clear previous
+
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.className = "episode-card";
+
+    const code = makeEpisodeCode(episode.season, episode.number);
+
+    card.innerHTML = `
+<div class="episode-title">${episode.name} – ${code}</div>
+<img src="${episode.image.medium}" alt="Episode image" />
+<div class="summary">${episode.summary}</div>
+<a href="${episode.url}" target="_blank">View on TVMaze</a>
+`;
+
+    rootElem.appendChild(card);
+  });
+}
+
+function makeEpisodeCode(season, episodeNum) {
+  const s = season.toString().padStart(2, "0");
+  const e = episodeNum.toString().padStart(2, "0");
+  return `S${s}E${e}`;
 }
 
 window.onload = setup;
